40ba6c352784b3ca96f4559bb5e5eb5b7cb432c7
HamChen <chenbinghan@gobestsoft.com>
2020-07-03 16:59:31 +0800

    update

--- config/config.ts
+++ config/config.ts
@@ -9,8 +9,11 @@ const { pwa } = defaultSettings;
 
 // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+console.log('process.env;', process.env)
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, REACT_APP_ENV } = process.env;
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
+console.log('process.env;11', REACT_APP_ENV)
+console.log('process.env;11',  isAntDesignProPreview)
 
 const plugins: IPlugin[] = [
   ['umi-plugin-antd-icon-config', {}],
--- src/pages/testModule/demo13/index.tsx
+++ src/pages/testModule/demo13/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './index.less';
-
+// import {Image} from 'antd';
 function styleList () {
     return (
         <div className={styles.contBox}>
@@ -11,6 +11,65 @@ function styleList () {
                 <li>电子邮件</li>
                 <li>通讯地址</li>
             </ul>
+          </div>
+          <div style={{marginTop: 50}}>
+            <div className={styles.crContent }>
+                <div style={{marginTop: 20}}>
+                    <div className={styles.hotUtil}>
+                        热门工具
+                    </div>
+                    <div className={styles.tool}>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//98e165b0d4d38f233d246adba25b5c89.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A50Z%2F2592000%2F%2Fe848a6eb9c253f8db7e62826b1db751fb975b6856316da2314736832693c8658" />
+                              <p className={styles.camp}>高考作文</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                        <div className={styles.spItem}>
+                            <a href="#" className={styles.link}>
+                              <img className={styles.imgIcon} src="https://search-operate.cdn.bcebos.com//8838beddf68a9853b53d604bccb180c2.png?authorization=bce-auth-v1%2Fc93fe3270045455693596cebb01c566e%2F2020-06-03T08%3A40%3A10Z%2F2592000%2F%2F7b84dbe8a83e5c0a865d9fa9147810d9f1dd486b84768bcb09645c369a81a987" />
+                              <p className={styles.camp}>历年真题</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
           </div>
         </div>
     );
