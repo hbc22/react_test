a4363e887402f76ad826c94bcb00f5caea7cb4e0
HamChen <chenbinghan@gobestsoft.com>
2020-08-13 15:14:32 +0800

    add_functi

--- src/pages/testModule/demo17/index.tsx
+++ src/pages/testModule/demo17/index.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from 'antd';
 import styles from './index.less';
 
 function markDemo () {
+    const [openit, setopenit] = useState(false)
+    const open = () => {
+        setopenit(true)
+    }
+    const colse = () =>{
+        setopenit(false)
+    }
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.alteradwarp}>
-                  <div className={styles.alterad}>
-                    <img className={styles.firimg} src="https://file2.speedtest.cn/images/ff74fda988261b8265747d346bc07ae5.png" alt=""/>
-                    <img className={styles.adimg} src="https://www.speedtest.cn/images/alter-close-other.png?6e453733bb19b52bc2128d7b955bab18" alt=""/>
-                  </div>
-                </div>
+                <Button onClick={open}>openit</Button>
+                {
+                    openit && (
+                        <div className={styles.alteradwarp}>
+                        <div className={styles.alterad}>
+                          <img className={styles.firimg} src="https://file2.speedtest.cn/images/ff74fda988261b8265747d346bc07ae5.png" alt=""/>
+                          <img onClick={colse} className={styles.adimg} src="https://www.speedtest.cn/images/alter-close-other.png?6e453733bb19b52bc2128d7b955bab18" alt=""/>
+                        </div>
+                      </div>
+                    )
+                }
             </div>
         </>
     )
